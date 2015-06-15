4022e602db4fa9088e1bee3874d3ff599c5c39f1
csouza <cleliofs@gmail.com>
2015-06-15 15:48:33 +0100

    Code refactoring

--- app/controllers/Application.scala
+++ app/controllers/Application.scala
@@ -3,6 +3,7 @@ package controllers
 import play.api.mvc._
 
 import scala.annotation.tailrec
+import scala.util.matching.Regex
 
 class Application extends Controller {
 
@@ -37,24 +38,23 @@ class Application extends Controller {
   }
 
 
-  def index(q: String) = {
-    println(q)
+  val q1 = """[\w\s:]+ largest: ([\d\s,]+)""".r
+  val q2 = """[\w\s:]+ ([\d]+) plus ([\d]+)""".r
+  val q3 = """[\w\s:]+ ([\d]+) multiplied by ([\d]+)""".r
+  val q4 = """[\w\s:]+ a square and a cube: ([\d\s,]+)""".r
+  val q5 = """[\w\s:]+ are primes: ([\d\s,]+)""".r
+  val q6 = """[\w]+: what colour is a ([\w]+)""".r
+  val q7 = """[\w]+: what currency did ([\w]+) use before the Euro""".r
+  val q8 = """[\w\s:]+ played ([\w\s]+) in the film Dr No""".r
+  val q9 = """[\w]+: which city is the ([\w\s]+) in""".r
+  val q10 = """[\w]+: who is the ([\w\s]+) of ([\w\s]+)""".r
+  val q11 = """\w+: what is the (\d+)(?:st|th|nd) and (\d+)(?:st|th|nd) number in the Fibonacci sequence""".r
+  val q12 = """[\w\s:]+ ([\d]+) minus ([\d]+)""".r
 
-    val q1 = """[\w\s:]+ largest: ([\d\s,]+)""".r
-    val q2 = """[\w\s:]+ ([\d]+) plus ([\d]+)""".r
-    val q3 = """[\w\s:]+ ([\d]+) multiplied by ([\d]+)""".r
-    val q4 = """[\w\s:]+ a square and a cube: ([\d\s,]+)""".r
-    val q5 = """[\w\s:]+ are primes: ([\d\s,]+)""".r
-    val q6 = """[\w]+: what colour is a ([\w]+)""".r
-    val q7 = """[\w]+: what currency did ([\w]+) use before the Euro""".r
-    val q8 = """[\w\s:]+ played ([\w\s]+) in the film Dr No""".r
-    val q9 = """[\w]+: which city is the ([\w\s]+) in""".r
-    val q10 = """[\w]+: who is the ([\w\s]+) of ([\w\s]+)""".r
-    val q11 = """\w+: what is the (\d+)(?:st|th|nd) number in the Fibonacci sequence""".r
-    val q12 = """[\w\s:]+ ([\d]+) minus ([\d]+)""".r
 
-    val response = q match {
-      case q1(numbers) => numbers.split(",").map(_.trim().toInt).max
+  def getResponse(q: String): String = {
+    q match {
+      case q1(numbers) => numbers.split(",").map(_.trim().toInt).max.toString
       case q2(num1, num2) => (num1.toInt + num2.toInt).toString
       case q3(num1, num2) => (num1.toInt * num2.toInt).toString
       case q4(numbers) => {
@@ -63,21 +63,24 @@ class Application extends Controller {
       }
       case q5(numbers) => {
         val res: Array[Int] = numbers.split(",").map(_.trim().toInt).filter(isPrime)
-        if (res.length > 0) res.mkString(",").toString else ""
+        if (res.length > 0) res.mkString(", ") else ""
       }
       case q6(fruit) => if (fruit == "banana") "yellow" else ""
       case q7(country) => if (country.toLowerCase == "Spain".toLowerCase) "Peseta" else ""
       case q8(actorName) => s"$actorName played Sean Connery"
       case q9(monument) => if (monument.toLowerCase == "eiffel tower") "Paris" else ""
       case q10(role, countryName) => if (role.toLowerCase == "prime minister" && countryName.toLowerCase == "great britan") "David Cameron" else ""
-      case q11(num) => fibSeq(num.toInt).last.toString
+      case q11(num1, num2) => Seq(fibSeq(num1.toInt).last.toString, fibSeq(num2.toInt).last.toString).mkString(", ")
       case q12(num1, num2) => (num1.toInt - num2.toInt).toString
       case _ => "Not answered"
     }
 
-    response
+  }
 
-//    Ok(response)
+
+  def index(q: String) = Action {
+    println(q)
+    Ok(getResponse(q))
   }
 
 }
@@ -94,7 +97,7 @@ object Application extends App {
 //  val q = "f8fed360: who played James Bond in the film Dr No"
 //  val q = "f8fed360: which city is the Eiffel Tower in"
 //  val q = "f8fed360: who is the Prime Minister of Great Britan"
-  val q = "f8fed360: what is the 14th number in the Fibonacci sequence"
-//    val q = "f8fed360: how much is 15 minus 10"
-  println(app.index(q))
+  val q = "f8fed360: what is the 1st and 2nd number in the Fibonacci sequence"
+//  val q = "f8fed360: how much is 15 minus 10"
+  println(app.getResponse(q))
 }
